efa03d4bc70d2ebffcb3344c2478cb14240b6d54
rabogan <71470525+rabogan@users.noreply.github.com>
2020-10-29 08:41:36 +0000

    Add files via upload

---
+++ Chapter6Exercise5/Source.cpp
@@ -0,0 +1,25 @@
+//This is a (hopefully correct) revised grammar!
+
+Sentence:
+	Noun Verb
+	Article Verb
+	Sentence Conjunction Sentence
+
+Article:
+	"The" Noun
+	"the" Noun
+
+Conjunction
+	"but"
+	"and"
+	"or"
+		
+Noun: 
+	"birds"
+	"fish"
+	"C++"
+
+Verb
+	"swim"
+	"fly"
+	"play"
